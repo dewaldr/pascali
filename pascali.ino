22536b26e955ab391efef3562be12d82c9bcdb56
Robot7 <dewald@robot7.co.za>
2017-02-13 17:46:56 +0200

    Updated Fritzing diagram

--- pascali.ino
+++ pascali.ino
@@ -20,16 +20,18 @@
 // - remove delays
 
 // User adjustable parameters
-float low  = 2.0; // MPa
-float high = 3.5; // MPa
+const float low  = 2.0; // MPa
+const float high = 3.5; // MPa
+const int pressurePin = A0;
+const int relayPin = 10;
 
 // Constants and code
 const int offset = 20;      // zero pressure adjust
 const int fullScale = 1023; // max pressure (span) adjust
 
 // TODO: pin as parameter
-float getPressure() {
-    float reading = analogRead(A0);
+float getPressure(const int pin) {
+    float reading = analogRead(pin);
     float result = (reading - offset) * 1.2 / (fullScale - offset); // MPa conversion
     return result;
 }
@@ -37,7 +39,7 @@ float getPressure() {
 // true = on, false = off (default)
 // TODO: pin as parameter
 //       check logic
-bool getRelayState() {
+bool getRelayState(const int pin) {
     bool state = false; // off
     return state;
 }
@@ -49,11 +51,11 @@ void setup() {
 void loop() {
 
     Serial.print("Pressure: ");
-    float pressure = getPressure();
+    float pressure = getPressure(pressurePin);
     Serial.print(pressure, 1);
     Serial.println(" MPa");
     Serial.print("pump state: ");
-    Serial.println(getRelayState == true ? "on" : "off");
+    Serial.println(getRelayState(relayPin) == true ? "on" : "off");
 
     delay(500);
 }
